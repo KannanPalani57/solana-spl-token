d83c0dcfe63d68dffcf13e7f93892c004da102a5
Kannan <kannanpalani455@gmail.com>
2024-04-18 13:54:00 +0530

    change metadata

--- src/mpl_metadata.ts
+++ src/mpl_metadata.ts
@@ -1,81 +1,122 @@
-
-import {Collection, CreateMetadataAccountV3InstructionAccounts, CreateMetadataAccountV3InstructionDataArgs, Creator, MPL_TOKEN_METADATA_PROGRAM_ID, UpdateMetadataAccountV2InstructionAccounts, UpdateMetadataAccountV2InstructionData, Uses, createMetadataAccountV3, updateMetadataAccountV2, findMetadataPda} from "@metaplex-foundation/mpl-token-metadata";
+import {
+  Collection,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionDataArgs,
+  Creator,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
+  UpdateMetadataAccountV2InstructionAccounts,
+  UpdateMetadataAccountV2InstructionData,
+  Uses,
+  createMetadataAccountV3,
+  updateMetadataAccountV2,
+  findMetadataPda,
+} from "@metaplex-foundation/mpl-token-metadata";
 import * as web3 from "@solana/web3.js";
-import { PublicKey, createSignerFromKeypair, none, signerIdentity, some } from "@metaplex-foundation/umi";
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { fromWeb3JsKeypair, fromWeb3JsPublicKey} from '@metaplex-foundation/umi-web3js-adapters';
-import fs from "fs"
+import {
+  PublicKey,
+  createSignerFromKeypair,
+  none,
+  signerIdentity,
+  some,
+} from "@metaplex-foundation/umi";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  fromWeb3JsKeypair,
+  fromWeb3JsPublicKey,
+} from "@metaplex-foundation/umi-web3js-adapters";
+import fs from "fs";
 
-export function loadWalletKey(): web3.Keypair {
+import dotenv from "dotenv"
+
+
+dotenv.config();
+
+
+export async function initializeKeypair(
+    connection: web3.Connection
+  ): Promise<web3.Keypair> {
     let keypair: web3.Keypair
-
+  
     if (!process.env.PRIVATE_KEY) {
       console.log("Creating .env file")
       keypair = web3.Keypair.generate()
       fs.writeFileSync(".env", `PRIVATE_KEY=[${keypair.secretKey.toString()}]`)
     } else {
-        let pkey = [235,134,42,95,180,111,208,25,7,28,100,61,220,72,128,254,50,204,109,231,13,205,249,62,136,8,195,145,82,158,52,203,99,243,82,149,193,79,85,183,140,246,164,122,141,138,223,62,211,33,207,217,1,95,102,193,50,141,18,12,19,223,254,162];
-      const secret = JSON.parse(JSON.stringify(pkey) ?? "") as number[]
+      const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
       const secretKey = Uint8Array.from(secret)
       keypair = web3.Keypair.fromSecretKey(secretKey)
     }
   
     console.log("PublicKey:", keypair.publicKey.toBase58())
+    // await airdropSolIfNeeded(keypair, connection)
     return keypair
   }
+  
 
-const INITIALIZE = true;
+const INITIALIZE = false;
 
-async function main(){
-    console.log("let's name some tokens in 2024!");
-    const myKeypair = loadWalletKey();
-    const mint = new web3.PublicKey("6bZxL4N1gY2jnJy6U1ztD8oy2XQQq4TRmy4s9ZpesMod");
+async function main() {
+  console.log("let's name some tokens in 2024!");
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
 
-    const umi = createUmi("https://api.devnet.solana.com");
-    const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair))
-    umi.use(signerIdentity(signer, true))
+  const myKeypair = await initializeKeypair(connection);
+  const mint = new web3.PublicKey(
+    "Fiwj4Jz3TyoTUg42RPzeCNAadNNVpcjjtHV4bJ2c2KDL"
+  );
 
-    const ourMetadata = { // TODO change those values!
-        name: "My Test Sol Token", 
-        symbol: "MYST",
-        uri: "https://raw.githubusercontent.com/KannanPalani57/solana-spl-token/main/src/metadata.json",
-    }
-    const onChainData = {
-        ...ourMetadata,
-        // we don't need that
-        sellerFeeBasisPoints: 0,
-        creators: none<Creator[]>(),
-        collection: none<Collection>(),
-        uses: none<Uses>(),
-    }
-    if(INITIALIZE){
-        const accounts: CreateMetadataAccountV3InstructionAccounts = {
-            mint: fromWeb3JsPublicKey(mint),
-            mintAuthority: signer,
-        }
-        const data: CreateMetadataAccountV3InstructionDataArgs = {
-            isMutable: true,
-            collectionDetails: null,
-            data: onChainData
-        }
-        const txid = await createMetadataAccountV3(umi, {...accounts, ...data}).sendAndConfirm(umi);
-        console.log(txid)
-    } else {
-        const data: UpdateMetadataAccountV2InstructionData = {
-            data: some(onChainData),
-            discriminator: 0,
-            isMutable: some(true),
-            newUpdateAuthority: none<PublicKey>(),
-            primarySaleHappened: none<boolean>()
-        }
-        const accounts: UpdateMetadataAccountV2InstructionAccounts = {
-            metadata: findMetadataPda(umi,{mint: fromWeb3JsPublicKey(mint)}),
-            updateAuthority: signer
-        }
-        const txid = await updateMetadataAccountV2(umi, {...accounts, ...data} ).sendAndConfirm(umi);
-        console.log(txid)
-    }
+  const umi = createUmi("https://api.devnet.solana.com");
+  const signer = createSignerFromKeypair(umi, fromWeb3JsKeypair(myKeypair));
 
+//   return;
+  umi.use(signerIdentity(signer, true));
+
+  const ourMetadata = {
+    // TODO change those values!
+    name: "Mitcome",
+    symbol: "MIT",
+    uri: "https://raw.githubusercontent.com/KannanPalani57/solana-spl-token/main/src/metadata.json",
+  };
+  const onChainData = {
+    ...ourMetadata,
+    // we don't need that
+    sellerFeeBasisPoints: 0,
+    creators: none<Creator[]>(),
+    collection: none<Collection>(),
+    uses: none<Uses>(),
+  };
+  if (INITIALIZE) {
+    const accounts: CreateMetadataAccountV3InstructionAccounts = {
+      mint: fromWeb3JsPublicKey(mint),
+      mintAuthority: signer,
+    };
+    const data: CreateMetadataAccountV3InstructionDataArgs = {
+      isMutable: true,
+      collectionDetails: null,
+      data: onChainData,
+    };
+    const txid = await createMetadataAccountV3(umi, {
+      ...accounts,
+      ...data,
+    }).sendAndConfirm(umi);
+    console.log(txid);
+  } else {
+    const data: UpdateMetadataAccountV2InstructionData = {
+      data: some(onChainData),
+      discriminator: 0,
+      isMutable: some(true),
+      newUpdateAuthority: none<PublicKey>(),
+      primarySaleHappened: none<boolean>(),
+    };
+    const accounts: UpdateMetadataAccountV2InstructionAccounts = {
+      metadata: findMetadataPda(umi, { mint: fromWeb3JsPublicKey(mint) }),
+      updateAuthority: signer,
+    };
+    const txid = await updateMetadataAccountV2(umi, {
+      ...accounts,
+      ...data,
+    }).sendAndConfirm(umi);
+    console.log(txid);
+  }
 }
 
 main();
